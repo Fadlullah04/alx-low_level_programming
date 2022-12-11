8dc4690a78e9e6a480841f7337f5a020f73f2f80
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-11 08:04:35 -0800

    yep i made it

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-	int n;
-	int last_number = n % 10;
+	int n; /* randomly generated number*/
+	int last_number = n % 10; /* holds the last digit of number generated */
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last_number = n % 10;
 	printf("Last digit of %d is %d", n, last_number);
 	if (last_number > 5)
 	{
