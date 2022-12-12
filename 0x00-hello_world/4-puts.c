aae4298584277b6b1255034de02560f46e0e193a
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-12 02:57:16 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * main - entry point of code
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\ "Programming is like building a multilingual puzzle);
+	return (0);
 }
