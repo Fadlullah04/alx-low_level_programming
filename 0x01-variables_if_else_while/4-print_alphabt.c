66c7694a7c2e7da41febb1ec528f85275c384f67
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-12 14:10:37 -0800

    program jumps e and q in the alphabet listing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	/*program prints a-z to stdout except e and q*/
+	int alphabets = 0;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets == 'e' || alphabets == 'q')
+			continue;
+			putchar (alphabets);
+	}
+	putchar('\n');
+	return (0);
+}
