3ad35e67e146f2a986bfbacf4d053f6fb35f8791
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-14 15:55:54 -0800

    print x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints the alphabets from a-z
+ *
+ *in lowercase
+ *
+ *Return: Nothing
+ */
+
+void print_alphabet_x10(void);
+{
+	int major = 0;
+
+	for (major = 0; major <= 10; major++)
+	{
+
+		int numcounter;
+
+		for (numcounter = 'a'; numcounter <= 'z'; numcounter++)
+		{
+			_putchar(numcounter);
+		}
+			_putchar('\n');
+	}
+}
+
+
