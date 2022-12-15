a54fd22a4f2458ba3932398d54ba6ee856d6eb0e
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-15 06:18:31 -0800

    added the fourth cfile

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+/**
+ * _islower - checks if a character is an alphabet
+ * @c: the character
+ * Return: 1 if letter is an alphabet, 0 if not
+ */
+
+int _isalpha(int c) 
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,11 @@
+#include"main.h"
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_isalpha(int a);
+	return (0);
+}
