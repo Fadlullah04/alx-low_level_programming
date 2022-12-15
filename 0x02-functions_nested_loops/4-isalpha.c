cd2ca564cfbd13795b1c765f95775114124834ea
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-15 06:21:57 -0800

    added the fourth cfile

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 /**
- * _islower - checks if a character is an alphabet
+ * _isalpha - checks if a character is an alphabet
  * @c: the character
  * Return: 1 if letter is an alphabet, 0 if not
  */
 
-int _isalpha(int c) 
+int _isalpha(int c)
 {
 	if (c >= 65 && c <= 122)
 	{
