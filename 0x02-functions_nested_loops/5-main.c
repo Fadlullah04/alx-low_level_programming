2fecd4f8155055ae5234543d3e428ea54237a509
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-16 11:16:50 -0800

    sign cfile

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * main - entry point of code
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{	
+	int print_sign(int 9);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_sign - prints sign of number n
+ * @param n: numerical input
+ * Return: 1 if n is positive 0 if n equals 0 and -1 if n is negtrive
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else 
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
+
