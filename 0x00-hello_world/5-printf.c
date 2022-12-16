0fdc8e266a09974ba0f92c53f84bcb6719bf3875
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-15 21:47:10 -0800

    print string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Succcess)
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art";
+
+	printf("%s\n", str);
+	return (0);
+}
