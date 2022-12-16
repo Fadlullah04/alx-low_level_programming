0edd38766f7b08b71ffd4a727d33af122fd487d1
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-15 22:30:58 -0800

    print out the sozes of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main -entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	return (0);
+}
+
