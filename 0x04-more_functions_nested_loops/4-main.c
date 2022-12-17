1f54396eb9d068b00f2d18756ded64029a22a943
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-17 07:39:56 -0800

    debugging

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**main - entry point of code
+ * Retirn: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_most_numbers - print 0 - 9 except 2 and 4
+ * Return: Nothing
+ */
+
+void print_most_numbers(void)
+{
+	int counter;
+
+	for (counter  = 48;  counter <= 57; counter++)
+	{
+		if (counter == 50  || counter == 59)
+			continue;
+			_putchar(counter);
+	}
+		_putchar('\n');
+}
