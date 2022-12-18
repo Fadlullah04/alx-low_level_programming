bf2f423e67be8c6a2d43024101d6ad4eb4bea25b
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-18 06:25:14 -0800

    debuig2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,8 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
+#ifndef MATH_H
+#define MATH_H
 #include <stdio.h>
 
 int largest_number(int a, int b, int c);
 
 void positive_or_negative(int i);
-
-
-
-#endif /* MAIN_H */
+#endif
