6e3d01051e2c74e870f7de531522f556a1ee23e1
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-18 02:45:43 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * main - tests functions that check the sign ogf an integer 
+ * Return:0
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or-negative(0);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include <stdio.h>
+
+void positive_or_negative(int 1);
+
+#endif
