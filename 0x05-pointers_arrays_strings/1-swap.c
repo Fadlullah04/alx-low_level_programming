4669672b674ca8add9c56c1569cde0da8cf89d10
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-19 07:52:51 -0800

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * swap_int  - swap values at address
+ * @a: first integer
+ * @b: second integer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	temp = *b;
+}
