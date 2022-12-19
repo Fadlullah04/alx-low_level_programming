d681c6375812b82c55777aaf1e5bb3c6d4e06d1f
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-19 10:09:44 -0800

    all

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_strlen - returns the lenght of a string
+ *@string: string
+ *Return: strimg
+ */
+
+int _strlen(char *string)
+{
+	return (strlen(string));
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-void reset_to_98(int *p);
+int _strlen(char *s);
 void swap_int(int *a, int *b);
+void reset_to_98(int *p);
 #endif
-
