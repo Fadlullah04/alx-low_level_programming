0ae02680461b4e3c3d4b310fbfc25d32708561dc
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-19 15:45:59 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+include"main.h"
+
+/**
+ * print_rev - reverses string
+ * @s: string inputed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int counter = 0;
+
+	while (str[counter] != '\0')
+	{
+		counter++;
+
+	for (int c = counter - 1; c >= 0, i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ void swap_int(int *a, int *b);
 void reset_to_98(int *p);
 void _puts(char *str);
 int _putchar(char c);
+void print_rev(char *s);
 #endif
