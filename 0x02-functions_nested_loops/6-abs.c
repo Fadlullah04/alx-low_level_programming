bf978a2ae55417278b50e7d78afb495a69b38968
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-20 00:33:39 -0800

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,23 @@
 #include"main.h"
+#include<stdio.h>
 
-/
+/**
+ * _abs - find the arsolute of a number
+ * @int: integrer value
+ * Return: result
+ */
 
+int _abs(int x) 
+{
+	int result = 0;
+
+	if (x < 0)
+	{
+		_putcahr(-1 * x);
+	}
+	else 
+	{
+		_putchar(x);
+	}
+	return (0);
+}
