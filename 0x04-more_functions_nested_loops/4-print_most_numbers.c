a6b2f0a3a5a0394f6ce38fcea3ae9ae3c80025d9
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-20 04:08:10 -0800

    as

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,8 @@ void print_most_numbers(void)
 	for (counter  = 48;  counter <= 57; counter++)
 	{
 	if (counter == 50  || counter == 59)
-	{	continue;
+		continue;
 		_putchar(counter);
 	}
-	}
-		_putchar('\n');
+	_putchar('\n');
 }
