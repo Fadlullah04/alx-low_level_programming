6276c47e0da4fd7e73d309e58de52a78ff78d270
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-20 05:41:08 -0800

    added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * puts2 - produce a random string from another starting with
+ * @str: string
+ * Return: void
+ */
+
+
+void puts2(char *str)
+{	
+	int i = 0;
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _strlen(char *s);
-void swap_int(int *a, int *b);
-void reset_to_98(int *p);
-void _puts(char *str);
 int _putchar(char c);
-void print_rev(char *s);
-void rev_string(char *s);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+int _isupper(int c);
+void more_numbers(void);
+void print_line(int n);
+void puts2(char *str);
 #endif
