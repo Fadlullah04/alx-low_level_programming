301811fd2d39453bdcf27d7ea6f0fca1fde94699
Fadlullah04 <fadlumuhd570@gmail.com>
2022-12-28 07:04:34 -0800

    ye

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,10 +11,11 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i++]){
-	if (str[i] >= 'a' && str[i] <= 'z')
+	while (str[i++])
 	{
-	str[i] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 	}
+
 	return (str);
 }
