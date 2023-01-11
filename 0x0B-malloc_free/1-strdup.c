f9995800323e0e07c07d9a6372019ba48bdbe873
Fadlullah04 <fadlumuhd570@gmail.com>
2023-01-11 03:54:22 -0800

    second malloc file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include<stddef.h>
+/*#include<string.h>*/
+
+/**
+ * _strdup - copy a string to a newly allocated space in memory
+ * @str: string to copy
+ * Return: pointer to duplicated string or NULL if error
+ */
+
+char *_strdup(char *str)
+{
+	char *str2;
+	size_t j;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+
+	str2 = malloc(sizeof(char) * i);
+
+	if (str2 == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		str2[j] = str[j];
+
+	return (str2);
+}
