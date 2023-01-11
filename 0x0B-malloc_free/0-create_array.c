60016d7721b50e6528c956cf0b825b3ca2ff7ff7
Fadlullah04 <fadlumuhd570@gmail.com>
2023-01-10 17:02:17 -0800

    Hot fix1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,12 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 	size_t i;
+	
+	if (size == 0)
+		return (NULL);
 
 	array = (char *)malloc(sizeof(char) * size);
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
 
