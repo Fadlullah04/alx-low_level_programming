4522d5d0c1f57609ae8a001778903e682fdb1133
Fadlullah04 <fadlumuhd570@gmail.com>
2023-05-24 10:46:32 -0700

    Complexes

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * print_to_98 - print from n to 98
+ * @n: starting point
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int last_num = 98;
+
+	if (n > 98)
+	{
+		for (; n >= 97; n--)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d", last_num);
+		putchar('\n');
+	}
+	else
+	{
+		for (; n <= 97; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d", last_num);
+		putchar('\n');
+	}
+}
