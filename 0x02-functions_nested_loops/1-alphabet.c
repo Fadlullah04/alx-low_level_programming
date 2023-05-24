6acc875ed31b33ad9460da3283944d571383d3a7
Fadlullah04 <fadlumuhd570@gmail.com>
2023-05-23 18:57:16 -0700

    Added 2nd file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i = 0;
+
+	while (i <= 122)
+	{
+		i = 97;
+		_putchar(i);
+		i++;
+	}
+}
