0b0f42baae6fd1606a37ec08f76d32d557c909e8
Fadlullah04 <fadlumuhd570@gmail.com>
2023-05-24 19:57:06 -0700

    added files

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * times_table - print the 9x table
+ */
+
+void times_table(void)
+{
+	int def, mulr, prod;
+
+	for (def = 0; def <= 9; def++)
+	{
+		_putchar('0');
+		for (mulr = 1; mulr <= 9; mulr++)
+		{
+			_putchar(44);
+			_putchar(' ');
+			prod = mulr * def;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
