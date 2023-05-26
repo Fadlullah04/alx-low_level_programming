b59cc0c526e10c8decfba2b45f95d096d6d63c15
Fadlullah04 <fadlumuhd570@gmail.com>
2023-05-26 09:52:48 -0700

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,6 @@
 
 int main(void)
 {
-	char *string = "and that piece of art is "
-		"useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, string, strlen(string));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",60);
 	return (1);
 }
