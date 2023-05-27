be40afcc5e97a67b212e334255af98c175e8c46b
Fadlullah04 <fadlumuhd570@gmail.com>
2023-05-27 10:00:53 -0700

    Updating 0x04...

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_diagonal - print a diagonal
+ * @n: length n
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+	
+	if (n <= 0)
+		_putchar('\n');
+	for (j = 0; j < n; j++)
+	{
+		for (i = 0; i <= j; i++)
+		{
+			_putchar(' ');
+		}
+		if (i == j)
+		{
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
