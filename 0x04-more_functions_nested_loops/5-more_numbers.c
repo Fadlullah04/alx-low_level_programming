c14c6f86cb71e5251bffe28d7e95f214fcfca5dd
Fadlullah04 <fadlumuhd570@gmail.com>
2023-05-27 07:07:03 -0700

    files added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * more_numbers - print 0 to 14 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + ')');
+		}
+		_putchar('\n');
+	}
+}
