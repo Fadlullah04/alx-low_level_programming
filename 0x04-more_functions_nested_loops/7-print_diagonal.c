5a0448ad03f3d1174b144830d9e465f08c5f1eee
Fadlullah04 <fadlumuhd570@gmail.com>
2023-05-27 10:18:00 -0700

    Updating 0x04...

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,8 +10,8 @@ void print_diagonal(int n)
 	
 	if (n <= 0)
 		_putchar('\n');
-	else
-	{	
+	/*else
+	{	*/
 		for (j = 0; j < n; j++)
 		{
 			for (i = 0; i < j; i++)
@@ -22,5 +22,5 @@ void print_diagonal(int n)
 				_putchar('\n');
 			}
 		}
-	}
+	/*}*/
 }
