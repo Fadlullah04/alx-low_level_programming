eeb55eddd0a54878d5efa74403cf21cd62c09617
Fadlullah04 <fadlumuhd570@gmail.com>
2023-05-30 17:27:21 -0700

    Night duty

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+	char *str;
+
+	str = s;
+	len = 0;
+	while (*str != '\0')
+	{
+		++len;
+		str++;
+	}
+
+	for (i = len; i > 0; i--)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
