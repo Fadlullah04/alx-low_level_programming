dd4311a977a56e5b19752d5fa3c86dc88322fbe1
Fadlullah04 <fadlumuhd570@gmail.com>
2023-06-05 09:20:43 -0700

    Created file 6-puts2.c and updated main.h

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,17 @@
-#include"main.h"
-
+#include "main.h"
 /**
  * puts2 - prints every other character of a string
- * @str: the string to be tested
+ *
+ * @str: char to check
+ *
+ * Return: 0 is success
  */
-
 void puts2(char *str)
 {
+	int string;
 
-	while (*str != '\0')
-	{
-		if (*str == '.')
-			break;
-		_putchar(*str);
-		str += 2;
-	}
+	for (string = 0; str[string] != '\0'; string++)
+	if (string % 2 == 0)
+		_putchar(str[string]);
 	_putchar('\n');
 }
