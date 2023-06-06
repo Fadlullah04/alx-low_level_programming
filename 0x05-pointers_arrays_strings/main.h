35541940b81509b8f021168993cf19705bc8f47e
Fadlullah04 <fadlumuhd570@gmail.com>
2023-06-06 14:17:59 -0700

    created file 9-strcpy and updated main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include<stdio.h>
+#include<string.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,4 +12,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 #endif /*MAIN_H*/
