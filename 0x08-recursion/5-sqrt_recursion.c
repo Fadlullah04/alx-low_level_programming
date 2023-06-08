d4683a5e03a4d54135117c81868966185176106b
Fadlullah04 <fadlumuhd570@gmail.com>
2023-06-08 16:31:49 -0700

    created files

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,8 +14,8 @@ int check(int a, int b)
 		return (a);
 	else if (a * a > b)
 		return (-1);
-	else 
-		return (check(a + 1, b);
+	else
+		return (check(a + 1, b));
 }
 
 
@@ -28,6 +28,6 @@ int check(int a, int b)
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
-		return(0);
+		return (0);
 	return (check(1, n));
 }
