0631a9e200a840b8e5cd6683489e386229f46d13
Fadlullah04 <fadlumuhd570@gmail.com>
2023-06-08 04:19:01 -0700

    new project: RECURSION

---
+++ 0x08-recursion/0-puts-recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _puts_recursion - print a string
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s == NULL || *s == '\0')
+		return;
+
+	_putchar(*s);
+	(_puts_recursion(s + 1));
+	_putchar('\n');
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void _puts_recursion(char *s);
+int _putchar(char c);
+#endif /*MAIN_H*/
