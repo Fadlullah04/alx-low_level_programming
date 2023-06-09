8ab388d0fa7064b17dd2874dc0c0561dbbc1a90e
Fadlullah04 <fadlumuhd570@gmail.com>
2023-06-08 18:06:42 -0700

    Update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * check - check for prime numbers
+ * @a: case tester I
+ * @b: case tester II
+ * Return: prime
+ */
+
+int check(int a, int b)
+{
+	if (b < 2 || b % a == 0)
+		return (0);
+	else if (a > b / 2)
+		return (1);
+	else
+		return (check(a + 1, b);
+}
+
+/**
+ * is_prime_number - check for a prime number
+ * @n: number to be tested
+ * Return: 1 if n is prime and 0 if otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (check(2, n);
+}
