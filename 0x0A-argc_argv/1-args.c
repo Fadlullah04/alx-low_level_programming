9fe365529675d7cf4aebc630d4fb0f20114530c5
Fadlullah04 <fadlumuhd570@gmail.com>
2023-06-14 03:14:29 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * main - get the number og arguments passed
+ * @argc - argument count
+ * @argv - argumnet vector
+ * Return: success
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", --argc);
+	return (0);
+}
