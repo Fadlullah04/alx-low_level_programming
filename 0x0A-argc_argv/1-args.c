78801b268d4e82cc00295bcee35fb1aed71a5a28
Fadlullah04 <fadlumuhd570@gmail.com>
2023-07-07 10:30:35 -0700

    created files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints number iof arguments passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
