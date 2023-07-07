c90583d29b0c3a1fb70c9645e23aefef6ff5b7c8
Fadlullah04 <fadlumuhd570@gmail.com>
2023-07-07 06:58:29 -0700

    advanced function complete

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,36 @@
 #include"main.h"
 
 /**
+ * palindrome_checker - compares individual characters
+ * @s: string to be checked
+ * and returns 1 or 0 to is_palindrome
+ * @start: beginning of string
+ * @end: end of string
+ * Return: 1 if palindrome is found
+ * 0 otherwise
+ */
+
+
+int palindrome_checker(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (palindrome_checker(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - checks a string if it's a palindrome.
  * @s: string to be checked
  * Return: 1 if s is a palindrome
- * 0 otherwiae
+ * 0 otherwise
  */
+
 
 int is_palindrome(char *s)
 {
-	if (s == NULL || *s == '\0')
-		return (1);
-	if (strlen(s) == 1)
-		return (1);
-	if (*s != s[strlen(s) - 1])
-		return (0);
-	*(s + strlen(s) - 1) = '\0';
-	return (is_palindrome(s + 1));
+	int len = strlen(s);
+
+	return (palindrome_checker(s, 0, len - 1));
 }
