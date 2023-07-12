681b4c72aa31a98b8c25616838658297f3f171de
Fadlullah04 <fadlumuhd570@gmail.com>
2023-07-12 01:52:32 -0700

    Updatred the 1-strdup fle

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * _strdup - diplicate a string
+ * @str: string
+ * Return: pointer to newly allocated memeory containing
+ * the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+	int len;
+
+	len = strlen(str);
+	if (str == NULL)
+		return (NULL);
+
+	p = malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		p[i] = str[i];
+	p[len] = '\0';
+	return (p);
+	free(p);
+}
