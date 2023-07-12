e7a55c406d4c9cc78a26bde13f98afe9d5c32bdc
Fadlullah04 <fadlumuhd570@gmail.com>
2023-07-12 03:29:20 -0700

    Added file 2-str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include"main.h"
+
+/**
+ * str_concat - concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: a pointer to the duplicated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i, len1, len2, len3;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	len3 = len1 + len2;
+
+	p = malloc(sizeof(char) * (len3 + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		p[i] = s1[i];
+	for (i = 0; i < len2; i++)
+	{
+		p[len1 + i] = s2[i];
+	}
+	p[len3] = '\0';
+	return (p);
+	free(p);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,6 +5,7 @@
 #include<string.h>
 #include<stdlib.h>
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 
 #endif /*MAIN_H*/
