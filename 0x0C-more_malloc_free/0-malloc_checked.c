04656f05cc9061a9c2d455bc14c4407322d8fb4b
Fadlullah04 <fadlumuhd570@gmail.com>
2023-07-14 06:07:57 -0700

    corrected

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,9 @@ void *malloc_checked(unsigned int b)
 {
 	void *p;
 
-	if (b > UINT_MAX)
-		exit(98);
-	p = malloc(sizeof(void *) * b);
+	p = malloc(b);
 	if (p == NULL)
 		exit(98);
+
 	return (p);
 }
